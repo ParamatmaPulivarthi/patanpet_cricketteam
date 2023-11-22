da9b85cbf4ea8cc3891b5f82045d4b8c7c71623a
mrs shameem <mrs.shameem352@gmail.com>
2023-11-22 21:24:44 +0530

    modified file

--- crickteam.py
+++ crickteam.py
@@ -3,6 +3,12 @@ print("patan pet cricket team bating person1: ", "gopi")
 print("patan pet cricket team bating person1: ", "manikanta")
 print("patan pet cricket team bating person1: ", "thrinadh")
 print("patan pet cricket team bating person1: ", "ganesh")
+bowler_names_in my_friends
+print("patan pet cricket team bating person1:","rahamathunisa")
+print("patan pet cricket team bating person1:","shaheena")
+print("patan pet cricket team bating person1:","meghana")
+print("patan pet cricket team bating person1:","sony")
+print("patan pet cricket team bating person1:","fathima")
 
 
 
