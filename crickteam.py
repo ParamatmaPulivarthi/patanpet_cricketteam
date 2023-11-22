d4b4f2cc4d2e32210520518e9ef4e2e4cc7b80ed
PARAMATMA PULIVARTHI <67418634+ParamatmaPulivarthi@users.noreply.github.com>
2023-11-22 20:37:49 +0530

    Created batting team details

---
+++ crickteam.py
@@ -0,0 +1,8 @@
+print("patan pet cricket team bating person1: ", "paramatma")
+print("patan pet cricket team bating person1: ", "gopi")
+print("patan pet cricket team bating person1: ", "manikanta")
+print("patan pet cricket team bating person1: ", "thrinadh")
+print("patan pet cricket team bating person1: ", "ganesh")
+
+
+
